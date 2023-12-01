0ef07479c094e58a0605935234472c7e363465b6
p.diego.carmo <carmopereiradiego.com>
2023-11-30 23:18:11 -0300

    Update seller and custom service exception

--- SalesWebMvc/Sevices/SellerService.cs
+++ SalesWebMvc/Sevices/SellerService.cs
@@ -1,6 +1,7 @@
 ﻿using SalesWebMVC.Data;
 using SalesWebMVC.Models;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Sevices.Exceptions;
 
 namespace SalesWebMVC.Sevices
 {
@@ -14,7 +15,7 @@ namespace SalesWebMVC.Sevices
 
         public List<Seller> FindAll()
         {
-            return _context.Seller.ToList(); 
+            return _context.Seller.ToList();
         }
 
         public void Insert(Seller obj)
@@ -33,6 +34,24 @@ namespace SalesWebMVC.Sevices
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if(!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
