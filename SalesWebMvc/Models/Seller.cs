1dbe8b10bc97145fc02b17b9f97b99521fd83f56
p.diego.carmo <carmopereiradiego.com>
2023-12-05 08:40:37 -0300

    validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -8,20 +8,36 @@ namespace SalesWebMVC.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be beetwen {2} and {1}")]
         [Display(Name = "Nome")]
         public string Name { get; set; }
-        //[DataType(DataType.EmailAddress)]
+
+
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Data de Nascimento")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
+
+
+        [Required(ErrorMessage = "{0} required")]
+        [Range(100.00, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Salario Base")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
+
+
         public Department Department { get; set; }
         [Display(Name = "Departamento")]
         public int DepartmentId { get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller() { }
